76cac2cb9f2c8dea8c19f4caa5fdc35e7976865f
Diana_Gallinger <92166687+gypsydiana@users.noreply.github.com>
2022-03-31 21:18:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include"Point.h";
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Point3Dh p1(6, 6, 6, 6);
+	Point3Dh p2(1, 2, 3, 4);
+	Point3Dh p3(100, 100, 101, 0);
+	cout << "_______________________" << endl;
+	cout << "                       |" << endl;
+	cout << "                       |" << endl;
+	cout << " Однородные координаты |" << endl;
+	cout << "                       |" << endl;
+	cout << "_______________________|" << endl;
+	cout << "Точка 1: (x=6, y=6, z=6, h=6)" << endl;
+	cout << "Точка 2: (x=1, y=2, z=3, h=4)" << endl;
+	cout << "Точка 3: (x=100, y=100, z=101, h=0)" << endl;
+	system("pause");
+	system("cls");
+	cout << "_______________________" << endl;
+	cout << "                       |" << endl;
+	cout << "                       |" << endl;
+	cout << " Реальные координаты   |" << endl;
+	cout << "                       |" << endl;
+	cout << "_______________________|" << endl;
+	cout << "Точка 1: ";
+	p1.real();
+	cout << endl;
+	cout << "Точка 2: ";
+	p2.real();
+	cout << endl;
+	cout << "Точка 3: ";
+	p3.real();
+}
